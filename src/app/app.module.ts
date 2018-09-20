8859206e6b07f6a0a4e0f7ed480d88ec2584bf31
lorenzohee <yongqinghee@163.com>
2018-09-20 14:27:20 +0800

    add core and shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared';
+import { MyNavComponent } from './my-nav/my-nav.component';
+import { NavigateComponent } from './core/navigate/navigate.component';
+import { CoreModule } from './core';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CoreModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,19 +1,97 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule  } from '@angular/forms';
-import { MatButtonModule } from '@angular/material';
+import {
+  MatAutocompleteModule,
+  MatBadgeModule,
+  MatBottomSheetModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatStepperModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule, } from '@angular/material';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { LayoutModule } from '@angular/cdk/layout';
 
 /* our own custom components */
 // import { SomeCustomComponent } from './some-custom/some-custom.component';
+
+const materialModule = [
+  LayoutModule,
+  MatAutocompleteModule,
+  MatBadgeModule,
+  MatBottomSheetModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatStepperModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule,
+]
 
 @NgModule({
   imports: [
     /* angular stuff */
     CommonModule,
     FormsModule,
+    /* 3rd party module */
+    ...materialModule
 
     /* 3rd party components */
-    MatButtonModule,
   ],
   declarations: [
     // SomeCustomComponent
@@ -24,7 +102,7 @@ import { MatButtonModule } from '@angular/material';
     FormsModule,
 
     /* 3rd party components */
-    MatButtonModule,
+    ...materialModule
 
     /* our own custom components */
     // SomeCustomComponent
